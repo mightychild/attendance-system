bc2784e7e4acd744c64247bfc6e6e8a054b9fdb0
mightychild <69825325+mightychild@users.noreply.github.com>
2025-06-23 01:26:03 +0100

    Attendance system

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StudentRegister from './components/Auth/StudentRegister';
 import Scanner from './components/Scanner';
